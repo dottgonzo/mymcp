a947c1e7b7db67b89f4f1df9a5af5da4c64ebaa6
dottg0n20 <dev@dariocaruso.info>
2025-09-07 02:03:43 +0200

    fix npx

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 import { MCPServer, type MCPServerConfig } from "mcp-framework";
+import { join } from "path";
 
 let mcpConfig: MCPServerConfig | undefined;
+const basePath = join(import.meta.dirname, "./index.js", "..");
 
 if (process.env.PORT) {
   mcpConfig = {
+    basePath,
     transport: {
       type: "http-stream",
       options: {
@@ -21,6 +24,10 @@ if (process.env.PORT) {
         },
       },
     },
+  };
+} else {
+  mcpConfig = {
+    basePath,
   };
 }
 
